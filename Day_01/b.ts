02c31f2f05ca849fc9530c281ba8f8293e0d8e74
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-12-01 23:43:21 +0000

    inspired by reddit

--- Day_01/b.ts
+++ Day_01/b.ts
@@ -2,44 +2,17 @@ const input:string = await Deno.readTextFile("./Day_01/b.txt");
 
 const lines = input.split("\n");
 
-const spelledOutNumbers = [
-{word:'one',number:'1'},
-{word:'two',number:'2'},
-{word:'three',number:'3'},
-{word:'four',number:'4'},
-{word:'five',number:'5'},
-{word:'six',number:'6'},
-{word:'seven',number:'7'},
-{word:'eight',number:'8'},
-{word:'nine',number:'9'},
-]
-
-function reverseString(str) {
-    return str.split("").reverse().join("");
-}
-
 const processedLines = lines.map((line) => {
-    const firstSpelledOutNumber = spelledOutNumbers
-        .map((spelledOutNumber) => ({...spelledOutNumber,position:line.indexOf(spelledOutNumber.word)}))
-        .filter((number) => number.position != -1)
-        .sort((a, b) => a.position - b.position)
-    [0]
-    const lastSpelledOutNumber = spelledOutNumbers
-        .map((spelledOutNumber) => ({...spelledOutNumber,position:line.lastIndexOf(spelledOutNumber.word)}))
-        .filter((number) => number.position != -1)
-        .sort((a, b) => b.position - a.position)
-    [0]
-
-    if (!firstSpelledOutNumber?.word) return line
-    const result = line
-        .replace(firstSpelledOutNumber.word, firstSpelledOutNumber.number)
-        .split("").reverse().join("") //because there is no replaceLast function
-        .replace(lastSpelledOutNumber.word.split("").reverse().join(""), lastSpelledOutNumber.number)
-        .split("").reverse().join("")
-    
-    // console.log(line, result)
-    return result
-
+    return line
+    .replaceAll('one','one1one')
+    .replaceAll('two','two2two')
+    .replaceAll('three','three3three')
+    .replaceAll('four','four4four')
+    .replaceAll('five','five5five')
+    .replaceAll('six','six6six')
+    .replaceAll('seven','seven7seven')
+    .replaceAll('eight','eight8eight')
+    .replaceAll('nine','nine9nine')
 })
 
 const characterLines = processedLines.map((line) => line.split(''))
@@ -48,10 +21,6 @@ const initialValue = 0
 const answer = characterLines.reduce((accumulator, charakterLine, i) => {
     const firstNumber = charakterLine.find((char) => Number(char))
     const lastNumber = charakterLine.findLast((char) => Number(char))
-    // console.log(lines[i], firstNumber, lastNumber)
-    if (charakterLine.indexOf(firstNumber!) == charakterLine.lastIndexOf(lastNumber!)) { //edge case: only 1 number
-        return accumulator + Number(firstNumber)
-    }
     return accumulator + Number(firstNumber + lastNumber)
 }, initialValue)
 
