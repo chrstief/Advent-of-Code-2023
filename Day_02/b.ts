0318d730553df731bee5887dffd2164c226ceb52
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-12-02 14:22:05 +0000

    day 2 b

---
+++ Day_02/b.ts
@@ -0,0 +1,44 @@
+const input: string = await Deno.readTextFile("./Day_02/b.txt");
+
+const lines = input.split("\n");
+
+type draw = {
+  red: number;
+  green: number;
+  blue: number;
+};
+
+type game = {
+  id: number;
+  draws: draw[];
+};
+
+function getGameID(line: string) {
+  return;
+}
+
+const allGames: game[] = lines.map((line) => {
+  const gameID = Number(line.match(/(?<=Game )[0-9]+/)![0]);
+  const draws: draw[] = line.split(";").map((drawString) => ({
+    red: Number(drawString.match(/\d+(?= red)/)?.[0]) || 0,
+    blue: Number(drawString.match(/\d+(?= blue)/)?.[0]) || 0,
+    green: Number(drawString.match(/\d+(?= green)/)?.[0]) || 0,
+  }));
+
+  return { id: gameID, draws: draws };
+});
+
+const fewestNeededCubes = allGames.map((game) => ({
+  maxRed: Math.max(...game.draws.map((draw) => draw.red)),
+  maxGreen: Math.max(...game.draws.map((draw) => draw.green)),
+  maxBlue: Math.max(...game.draws.map((draw) => draw.blue)),
+}));
+
+const initialValue = 0;
+const answer = fewestNeededCubes.reduce(
+  (accumulator, game) =>
+    accumulator + game.maxRed * game.maxGreen * game.maxBlue,
+  initialValue
+);
+
+console.log(answer);
