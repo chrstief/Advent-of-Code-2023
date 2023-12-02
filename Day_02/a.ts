d3a0e60544bca86ee8f0b0ab9d3e590ceef1a9aa
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-12-02 14:03:19 +0000

    Day 2 a

---
+++ Day_02/a.ts
@@ -0,0 +1,45 @@
+const input: string = await Deno.readTextFile("./Day_02/a.txt");
+
+const lines = input.split("\n");
+
+type draw = {
+  red: number;
+  green: number;
+  blue: number;
+};
+
+type game = {
+  id: number;
+  draws: draw[];
+};
+
+function getGameID(line: string) {
+  return;
+}
+
+const allGames: game[] = lines.map((line) => {
+  const gameID = Number(line.match(/(?<=Game )[0-9]+/)![0]);
+  const draws: draw[] = line.split(";").map((drawString) => ({
+    red: Number(drawString.match(/\d+(?= red)/)?.[0]) || 0,
+    blue: Number(drawString.match(/\d+(?= blue)/)?.[0]) || 0,
+    green: Number(drawString.match(/\d+(?= green)/)?.[0]) || 0,
+  }));
+
+  return { id: gameID, draws: draws };
+});
+
+const maxRed = 12;
+const maxGreen = 13;
+const maxBlue = 14;
+
+const possibleGames = allGames.filter((game) =>
+  game.draws.every(
+    (draw) =>
+      draw.red <= maxRed && draw.green <= maxGreen && draw.blue <= maxBlue
+  )
+);
+
+const initialValue = 0
+const answer = possibleGames.reduce((accumulator, game) => accumulator + game.id, initialValue)
+
+console.log(answer);
