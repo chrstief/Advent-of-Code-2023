cc9b267fe15d6cde09d3f9f3f8b58c324b89986f
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-12-06 06:10:18 +0000

    day 06 b

---
+++ Day_06/b.ts
@@ -0,0 +1,29 @@
+const input: string = await Deno.readTextFile("./Day_06/a.txt");
+
+const lines=input.split('\n')
+  
+const times =
+  [Number(lines[0].split(':')[1]?.replace(/\s*/g,""))]
+const distances =
+[Number(lines[1].split(':')[1]?.replace(/\s*/g,""))]
+
+const races = times?.map((time, index) => ({
+  time: time,
+  recordDistance: distances[index],
+}));
+// console.log(races)
+
+const result = races.reduce((accumulator, race) => {
+    const waysToWin: number[] = []
+    for (let buttonPressTime = 0; buttonPressTime <= race.time; buttonPressTime++){
+        const distanceTraveled = (race.time - buttonPressTime)*buttonPressTime
+        // console.log({buttonPressTime},{distanceTraveled})
+        if (distanceTraveled>race.recordDistance) waysToWin.push(buttonPressTime)
+    }
+    // console.log(waysToWin)
+    return accumulator*waysToWin.length
+},1)
+
+console.log(result);
+
+
