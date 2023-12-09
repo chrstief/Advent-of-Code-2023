d183c666185f26931be0d13fc697f33cb74538a4
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-12-09 06:59:50 +0000

    day 09

---
+++ Day_09/b.ts
@@ -0,0 +1,34 @@
+const input: string = await Bun.file("./Day_09/a.txt").text();
+const measurements = input
+  .split("\n")
+  .map((line) => line.split(" ").map((char) => Number(char)));
+
+// console.log(measurements)
+
+const answer = measurements
+  .map((measurement) => {
+    let differenceSequences = [measurement];
+    while (true) {
+      const differences = calcDifferences(
+        differenceSequences[differenceSequences.length - 1]
+      );
+      if (differences.every((difference) => difference === 0)) break;
+      differenceSequences.push(differences);
+    }
+    // console.log(differenceSequences);
+    const nextHistoryValue = differenceSequences.reduceRight(
+      (acc, differences) => differences[0] - acc,
+      0
+    );
+    // console.log(nextHistoryValue);
+    return nextHistoryValue;
+  })
+  .reduce((acc, nextHistoryValue) => acc + nextHistoryValue);
+console.log(answer);
+
+function calcDifferences(series: number[]) {
+  return Array.from(
+    { length: series.length - 1 },
+    (_, index) => series[index + 1] - series[index]
+  );
+}
