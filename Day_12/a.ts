396a7ae68f8112a3f147ba5490b54b1ec44fe0af
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-12-12 07:46:26 +0000

    refactor

--- Day_12/a.ts
+++ Day_12/a.ts
@@ -1,6 +1,10 @@
 const input: string = await Bun.file("./Day_12/a.txt").text();
 
-const rows = input.split("\n").map((row) => ({
+type Record = string;
+type Groups = number[];
+type Row = { record: Record; groups: Groups };
+
+const rows: Row[] = input.split("\n").map((row) => ({
   record: row.split(" ")[0],
   groups: row
     .split(" ")[1]
@@ -8,36 +12,29 @@ const rows = input.split("\n").map((row) => ({
     .map((group) => Number(group)),
 }));
 
-const arrangements = rows.map((row) => recursivelyTryCombinations(row));
-// console.log(arrangements);
-const answer = arrangements.reduce((acc, element) => acc + element)
-console.log(answer)
+const answer = rows
+  .map((row) => recursivelyTryCombinations(row.record, row.groups))
+  .reduce((acc, element) => acc + element);
+console.log(answer);
 
-
-function testRow(row: (typeof rows)[number]) {
-  const matches = row.record.match(/#+/g);
-  const lengths = matches?.map((match) => match.length) ?? [];
-  const sameLength = lengths.length == row.groups.length;
-  const sameElements = lengths.every(
-    (length, index) => row.groups[index] == length
-  );
-  // if (sameLength && sameElements) console.log(row.record);
-  return Number(sameLength && sameElements);
+function testRow(record: Record, groups: Groups) {
+  const damagedGroups = record.match(/#+/g)?.map((match) => match.length) ?? [];
+  if (
+    damagedGroups.length == groups.length &&
+    damagedGroups.every((length, index) => groups[index] == length)
+  )
+    return 1;
+  return 0;
 }
 
-function recursivelyTryCombinations(row: (typeof rows)[number]): number {
-  const hasUnknown = row.record.includes("?");
-  if (!hasUnknown) return testRow(row);
-  const nextSpringDamaged = {
-    ...row,
-    record: row.record.replace("?", "#"),
-  };
-  const nextSpringWorking = {
-    ...row,
-    record: row.record.replace("?", "."),
-  };
-  return (
-    recursivelyTryCombinations(nextSpringDamaged) +
-    recursivelyTryCombinations(nextSpringWorking)
-  );
+function recursivelyTryCombinations(record: Record, groups: Groups): number {
+  if (record.includes("?")) {
+    const nextSpringDamagedRecord = record.replace("?", "#");
+    const nextSpringWorkingRecord = record.replace("?", ".");
+    return (
+      recursivelyTryCombinations(nextSpringDamagedRecord, groups) +
+      recursivelyTryCombinations(nextSpringWorkingRecord, groups)
+    );
+  }
+  return testRow(record, groups);
 }
