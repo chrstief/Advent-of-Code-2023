bd03dacb3c7f76c0be43963976e823763a9b475c
chrstief <chrstief@gmail.com>
2023-12-14 22:25:50 +0000

    use cyclic states

--- Day_14/b.ts
+++ Day_14/b.ts
@@ -60,14 +60,8 @@ class Element {
 }
 
 function printPlatform(heading: string) {
-  const array: string[][] = Array.from({ length: rows }, () =>
-    Array.from({ length: columns }, () => ".")
-  );
-  stones.forEach((element) => {
-    array[element.row][element.column] = element.type;
-  });
   console.log(heading);
-  array.forEach((row, index) => console.log(index, row));
+  generatePlatform().forEach((row, index) => console.log(index, row));
 }
 
 function calculateWeight() {
@@ -78,8 +72,18 @@ function calculateWeight() {
   return weight;
 }
 
+function generatePlatform() {
+  const array: string[][] = Array.from({ length: rows }, () =>
+    Array.from({ length: columns }, () => ".")
+  );
+  stones.forEach((element) => {
+    array[element.row][element.column] = element.type;
+  });
+  return array;
+}
+
 //////////  module code  //////////
-const input: string = await Bun.file("./Day_14/a_sample.txt").text();
+const input: string = await Bun.file("./Day_14/a.txt").text();
 
 const allElements: Element[][] = input
   .split("\n")
@@ -95,8 +99,12 @@ const stones = allElements.flat().filter((element) => !(element.type == "."));
 // printPlatform("start");
 const roundStones = stones.filter((element) => element.type == "O");
 
-for (let cycle = 1; cycle <= 1000000000; cycle++) {
-  console.time('');
+const cycles: {
+  [key: string]: number[];
+} = {};
+
+for (let cycle = 1; cycle <= 126; cycle++) {
+  console.time("time elapsed");
   roundStones.sort((a, b) => a.row - b.row);
   roundStones.forEach((roundStone) => roundStone.moveNorth());
   // printPlatform("north");
@@ -108,9 +116,29 @@ for (let cycle = 1; cycle <= 1000000000; cycle++) {
   // printPlatform("south");
   roundStones.sort((a, b) => b.column - a.column);
   roundStones.forEach((roundStone) => roundStone.moveEast());
-  if (cycle % 100000 == 0) {
+  if (cycle % 10 == 0) {
+    // printPlatform(`Cycle ${cycle}`);
     console.log(`Cycle ${cycle}`);
-    console.timeEnd('');
+    console.timeEnd("time elapsed");
   }
+  // const snapshot = generatePlatform().flat().join("");
+  // // console.log(snapshot)
+  // if (cycles.hasOwnProperty(snapshot)) {
+  //   cycles[snapshot].push(cycle);
+  // } else {
+  //   cycles[snapshot] = [cycle];
+  // }
 }
+
+console.log("unique states: ", Object.keys(cycles).length);
+
+/*Looking at the snapshots from running a.sample.txt there are only 9 distinct states of the platform.
+After the first 2, the platform continues cycling through the next 7 states.
+(1000000000-2)%7 = 4. That means the platform will be in the 4th of the seven cyclic states,
+which corresponds to the state after 6 cycles. Therefore, we only need to calculate 6 cycles.
+
+The puzzle input has 149 unique states. Cycling starts after 115.
+(1000000000-115)%34 = 11. 115+11=126
+*/
+
 console.log(calculateWeight());
